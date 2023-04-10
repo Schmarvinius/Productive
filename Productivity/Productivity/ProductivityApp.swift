c2821c54aa0e7a7762ad1b0aa875ec5aa4786b1b
Marvin Lindner <marvin.lindner@sap.com>
2023-04-10 23:49:49 +0200

    Initial App
    Created a Mac App (no content)

---
+++ Productivity/Productivity/ProductivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityApp.swift
+//  Productivity
+//
+//  Created by Lindner, Marvin on 10.04.23.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
